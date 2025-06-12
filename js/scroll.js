d9c3a6d541242a2805dcaa321f3e8d946d60a916
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-06-12 21:04:24 +0330

    add router

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,25 @@
 import { getSerials } from "./api.js";
 import { renderMovies } from "./ui.js";
-
+import { isHome } from "./app.js";
 export function handleScroll() {
-    const scrollTop = window.scrollY;
-    const windowHeight = window.innerHeight;
-    const fullHeight = document.body.scrollHeight;
-    
-    if (scrollTop + windowHeight >= fullHeight - 0.5) {
-        loadMoreMovies();
-    }
+  if (!isHome) return;
+  const scrollTop = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const fullHeight = document.body.scrollHeight;
+
+  if (scrollTop + windowHeight >= fullHeight - 0.5) {
+    loadMoreMovies();
+  }
 }
 
 let counterPage = 0;
 let isGeveData = false;
 export async function loadMoreMovies() {
-    if (isGeveData) return;
-    isGeveData = true;
-    counterPage++;
-    try {
-      const sectionParent = document.querySelector(".render-ipsodes");
+  if (isGeveData) return;
+  isGeveData = true;
+  counterPage++;
+  try {
+    const sectionParent = document.querySelector(".render-ipsodes");
     const dataApi = await getSerials(counterPage);
     dataApi.forEach((data) => renderMovies(data, sectionParent));
   } catch (error) {
